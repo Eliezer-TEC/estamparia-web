14ddefebbc0c83d374866b8205e74f462983bcc8
vitor <vitorgarciadevasconcelos@gmail.com>
2024-01-23 14:25:14 -0300

    Alterado listagem de pedido

---
+++ src/app/booleanToYesNo.pipe.ts
@@ -0,0 +1,10 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'booleanToYesNo'
+})
+export class BooleanToYesNoPipe implements PipeTransform {
+  transform(value: boolean): string {
+    return value ? 'SIM' : 'NÃO';
+  }
+}
